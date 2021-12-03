18f29eae817567bb3a4c50450c83b129f341a917
kajal-developer <ch.kajal100@gmail.com>
2021-12-03 16:50:59 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component, VERSION } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular ' + VERSION.major;
+  myComapny = "Coforge India Limited";
+
+  companyAddr = "Greater Noida";
+
+  disableStat = false;
+  months = ["January", "February", "March", "April", "May", "June", "July","August", "September", "October", "November", "December"];    selectedMonth: any;
+    changemonths(event: any) {
+
+      alert("Changed month from the Dropdown : " + (this.selectedMonth));
+
+   }
+   myClickFunction(event: any) {
+
+    alert("Button is clicked");
+
+    console.log(event);
+
+ }
 }
